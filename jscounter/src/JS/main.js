555d7aa7c697c057000291d88ff856740e19ff47
shuaibcedcoss <126545931+mohd01032023@users.noreply.github.com>
2023-03-13 18:49:10 +0530

    Update main.js

--- jscounter/src/JS/main.js
+++ jscounter/src/JS/main.js
@@ -1,17 +1,7 @@
+let val = document.getElementById("input").value;
 function dec(){
-    var val = document.getElementById("input").value;
     if(val != 0){
-        var val = document.getElementById("input").value;
-        val--;
-        document.getElementById("input").value = val;
-    } 
-    else{
-        document.getElementById("input").value = 0;
+        document.getElementById("input").value = --val;
     }
 }
-
-function inc(){
-    var val = document.getElementById("input").value;
-    val++;
-    document.getElementById("input").value = val;
-}
+let inc = () => document.getElementById("input").value = ++val;
